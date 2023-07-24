373bfe5a6daf27de84b4bb9da779652a60d81efc
Ishaan1311 <92614091+Ishaan1311@users.noreply.github.com>
2023-07-24 20:27:25 +1200

    Add files via upload

---
+++ move_files.py
@@ -0,0 +1,28 @@
+import os 
+import shutil
+
+from_dir = "C:/Users/ishaa/Downloads"
+to_dir = "C:/Users/ishaa/Downloads/Drivers"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+for file_name in list_of_files:
+    name, extension = os.path.splitext(file_name)
+    print(name)
+    print(extension)
+    if extension == '':
+        continue
+    if extension in  ['.txt','.pdf','.doc','.docx']:
+        path1 = from_dir + '/' + file_name
+        path2 = to_dir + '/' + "Image_files"
+        path3 = to_dir + '/' + "Image_Files" + '/' + file_name
+        print ("path1",path1)
+        print ("path3",path3)
+        if os.path.exists(path2):
+            print("Moving" + file_name + ".....")
+            shutil.move(path1, path3)
+        else:
+            os.makedirs(path2)
+            print("Moving" + file_name + ".....")
+            shutil.move(path1,path3)
